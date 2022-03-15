cd396c53c4e43be3343092210d3b35f81a33e02a
rhirabay <rhirabay@yahoo-corp.jp>
2022-03-15 19:32:45 +0900

    update

---
+++ java/spring-boot/newrelic/src/test/java/rhirabay/newrelic/NewrelicApplicationTests.java
@@ -0,0 +1,13 @@
+package rhirabay.newrelic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewrelicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
