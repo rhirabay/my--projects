4184cfbc0c9a7b43d53dc7bd076691072cadc376
rhirabay <rhirabay@yahoo-corp.jp>
2022-03-15 09:42:17 +0900

    update

--- java/openrewire/openrewrite-sample/my-recipes/src/main/java/rhirabay/recipe/SayHelloRecipe.java
+++ java/openrewire/openrewrite-sample/my-recipes/src/main/java/rhirabay/recipe/SayHelloRecipe.java
@@ -1,24 +1,12 @@
 package rhirabay.recipe;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.extern.slf4j.Slf4j;
 import org.openrewrite.ExecutionContext;
 import org.openrewrite.Recipe;
-import org.openrewrite.internal.lang.NonNull;
 import org.openrewrite.java.JavaIsoVisitor;
 import org.openrewrite.java.JavaTemplate;
 import org.openrewrite.java.tree.J;
 
 public class SayHelloRecipe extends Recipe {
-//    @NonNull
-//    private final String fullyQualifiedClassName;
-
-//    @JsonCreator
-//    public SayHelloRecipe(@NonNull @JsonProperty("fullyQualifiedClassName") String fullyQualifiedClassName) {
-//        this.fullyQualifiedClassName = fullyQualifiedClassName;
-//    }
-
     @Override
     public String getDisplayName() {
         return "Say Hello";
@@ -36,7 +24,6 @@ public class SayHelloRecipe extends Recipe {
 
         @Override
         public J.ClassDeclaration visitClassDeclaration(J.ClassDeclaration classDecl, ExecutionContext executionContext) {
-            // Check if the class already has a method named "hello" so we don't incorrectly add a second "hello" method
             boolean helloMethodExists = classDecl.getBody().getStatements().stream()
                     .filter(statement -> statement instanceof J.MethodDeclaration)
                     .map(J.MethodDeclaration.class::cast)
