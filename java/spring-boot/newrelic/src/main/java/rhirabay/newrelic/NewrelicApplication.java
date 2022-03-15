49228dbd874d3b49222db976a7de50c7b0f26827
rhirabay <rhirabay@yahoo-corp.jp>
2022-03-15 19:32:45 +0900

    update

---
+++ java/spring-boot/newrelic/src/main/java/rhirabay/newrelic/NewrelicApplication.java
@@ -0,0 +1,13 @@
+package rhirabay.newrelic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewrelicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewrelicApplication.class, args);
+	}
+
+}
