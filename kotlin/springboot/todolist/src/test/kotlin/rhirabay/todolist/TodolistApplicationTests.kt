0d0a64c0c63ea976181a95344f5060c2ac16c362
rhirabay <rhirabay@yahoo-corp.jp>
2022-03-19 21:38:51 +0900

    update

--- kotlin/springboot/todolist/src/test/kotlin/rhirabay/todolist/TodolistApplicationTests.kt
+++ kotlin/springboot/todolist/src/test/kotlin/rhirabay/todolist/TodolistApplicationTests.kt
@@ -1,13 +1,28 @@
 package rhirabay.todolist
 
+import com.ninjasquad.springmockk.MockkBean
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit.jupiter.SpringExtension
+import org.springframework.test.web.reactive.server.WebTestClient
 
-@SpringBootTest
+@SpringBootTest(classes = [TodolistApplication::class],
+		webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class TodolistApplicationTests {
+	@Autowired
+	lateinit var webTestClient: WebTestClient
 
 	@Test
-	fun contextLoads() {
+	fun `integration test`() {
+		webTestClient.get()
+				.uri("/api/todo")
+				.exchange()
+				.expectStatus().isOk
 	}
+
 
 }
