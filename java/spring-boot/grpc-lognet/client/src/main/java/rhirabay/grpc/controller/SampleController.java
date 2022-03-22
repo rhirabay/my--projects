be456a25486205bc3f5eb4c5446c6d10fd926c9d
rhirabay <rhirabay@yahoo-corp.jp>
2022-03-22 22:46:39 +0900

    update

--- java/spring-boot/grpc-lognet/client/src/main/java/rhirabay/grpc/controller/SampleController.java
+++ java/spring-boot/grpc-lognet/client/src/main/java/rhirabay/grpc/controller/SampleController.java
@@ -5,14 +5,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import rhirabay.grpc.client.SampleClient;
+import rhirabay.grpc.client.TlsSampleClient;
 
 @RestController
 @RequiredArgsConstructor
 public class SampleController {
     private final SampleClient sampleClient;
+    private final TlsSampleClient tlsSampleClient;
 
     @GetMapping("/greeting")
     public String greeting(@RequestParam(defaultValue = "anonymous", required = false) String name) {
         return sampleClient.greeting(name);
+    }
+
+    @GetMapping("/tls-greeting")
+    public String tlsGreeting(@RequestParam(defaultValue = "anonymous", required = false) String name) {
+        return tlsSampleClient.greeting(name);
     }
 }
