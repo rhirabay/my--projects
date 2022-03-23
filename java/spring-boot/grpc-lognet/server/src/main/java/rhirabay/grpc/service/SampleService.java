2b43747b5be001094c4a1ffe3f0caa527ad17c1f
rhirabay <rhirabay@yahoo-corp.jp>
2022-03-23 13:15:01 +0900

    update

--- java/spring-boot/grpc-lognet/server/src/main/java/rhirabay/grpc/service/SampleService.java
+++ java/spring-boot/grpc-lognet/server/src/main/java/rhirabay/grpc/service/SampleService.java
@@ -1,14 +1,17 @@
 package rhirabay.grpc.service;
 
 import io.grpc.stub.StreamObserver;
+import io.micrometer.core.annotation.Timed;
 import org.lognet.springboot.grpc.GRpcService;
 import rhirabay.grpc.sample.GreetGrpc;
 import rhirabay.grpc.sample.GreetRequest;
 import rhirabay.grpc.sample.GreetResponse;
 
+// @GRpcServiceを付与すると、grpcのserviceとして認識される
 @GRpcService(interceptors = SampleInterceptor.class)
 public class SampleService extends GreetGrpc.GreetImplBase {
     @Override
+    @Timed(percentiles = { 0.5, 0.99 })
     public void greeting(GreetRequest request, StreamObserver<GreetResponse> responseObserver) {
         GreetResponse response = GreetResponse.newBuilder()
                 .setMessage("Hello, " + request.getName() + ".")
