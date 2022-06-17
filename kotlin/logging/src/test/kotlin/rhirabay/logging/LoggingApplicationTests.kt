d402db0a48df83e4bc70efc325c4e4ab83878907
rhirabay <nobody@dummy.com>
2022-06-17 14:15:06 +0900

    update

---
+++ kotlin/logging/src/test/kotlin/rhirabay/logging/LoggingApplicationTests.kt
@@ -0,0 +1,13 @@
+package rhirabay.logging
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LoggingApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
