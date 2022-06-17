77ffb3bffe68b0fb0976792a52197439a6c2bf9c
rhirabay <nobody@dummy.com>
2022-06-17 14:15:06 +0900

    update

---
+++ gradle/lighthouse/src/main/java/rhirabay/gradle/lighthouse/SampleController.java
@@ -0,0 +1,18 @@
+package rhirabay.gradle.lighthouse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class SampleController {
+    @GetMapping("/hello")
+    public String hello(
+            @RequestParam(required = false, defaultValue = "anonymous") String name,
+            Model model
+    ) {
+        model.addAttribute("name", name);
+        return "hello";
+    }
+}
