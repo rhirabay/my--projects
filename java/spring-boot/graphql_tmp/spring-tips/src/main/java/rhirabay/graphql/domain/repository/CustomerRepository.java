f9be92ab639c7931db625a960dc699328bbcb92a
rhirabay <rhirabay@yahoo-corp.jp>
2022-07-19 09:07:17 +0900

    update

--- java/spring-boot/graphql_tmp/spring-tips/src/main/java/rhirabay/graphql/domain/repository/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package rhirabay.graphql.domain.repository;
-
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-import reactor.core.publisher.Flux;
-import rhirabay.graphql.domain.model.Customer;
-
-public interface CustomerRepository extends ReactiveCrudRepository<Customer, Integer> {
-    Flux<Customer> findByName(String name);
-}
