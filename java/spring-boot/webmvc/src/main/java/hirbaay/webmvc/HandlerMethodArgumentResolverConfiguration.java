174bff2df5f945683cd28c372de25724815d447e
rhirabay <rhirabay@yahoo-corp.jp>
2023-02-10 19:11:22 +0900

    add project

---
+++ java/spring-boot/webmvc/src/main/java/hirbaay/webmvc/HandlerMethodArgumentResolverConfiguration.java
@@ -0,0 +1,16 @@
+package hirbaay.webmvc;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Configuration
+public class HandlerMethodArgumentResolverConfiguration implements WebMvcConfigurer {
+    // 実装したresolverを登録する
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+        argumentResolvers.add(new SampleArgumentResolver());
+    }
+}
