50457864334e549a21d9506fb35fc6fa77742dfb
rhirabay <rhirabay@yahoo-corp.jp>
2023-02-13 23:42:54 +0900

    add project

---
+++ java/spring-boot/library/springboot-lib/src/main/java/hirabay/library/springboot/MathUtils.java
@@ -0,0 +1,7 @@
+package hirabay.library.springboot;
+
+public class MathUtils {
+    public int plus(int n1, int n2) {
+        return n1 + n2;
+    }
+}
---
+++ java/spring-boot/springdoc/springboot2/webmvc/src/main/java/hirabay/springdoc/SampleControllerAdvice.java
@@ -0,0 +1,18 @@
+package hirabay.springdoc;
+
+import hirabay.springdoc.model.CommonErrorResponse;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
+public class SampleControllerAdvice {
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public CommonErrorResponse handleException() {
+        return new CommonErrorResponse();
+    }
+}
