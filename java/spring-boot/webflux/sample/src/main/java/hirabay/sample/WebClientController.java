6d9d32c6636956f3d2384fca8509d709acec164b
rhirabay <rhirabay@yahoo-corp.jp>
2023-02-21 09:02:13 +0900

    update

---
+++ java/spring-boot/webflux/sample/src/main/java/hirabay/sample/WebClientController.java
@@ -0,0 +1,67 @@
+package hirabay.sample;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.util.UriBuilder;
+import org.springframework.web.util.UriComponentsBuilder;
+import reactor.core.publisher.Mono;
+
+import java.util.UUID;
+
+@RestController
+@RequiredArgsConstructor
+public class WebClientController {
+    private final WebClient.Builder webClientBuilder;
+
+    @GetMapping("/webclient/ng1")
+    public Mono<String> ng1() {
+        return webClientBuilder
+                .baseUrl("http://localhost:8080")
+                .build()
+                .get()
+                .uri("/api/sample?uuid=" + UUID.randomUUID().toString())
+                .retrieve()
+                .bodyToMono(String.class);
+    }
+
+    @GetMapping("/webclient/ng2")
+    public Mono<String> ng2() {
+        return webClientBuilder
+                .baseUrl("http://localhost:8080")
+                .build()
+                .get()
+                .uri(uriBuilder -> uriBuilder.path("/api/sample")
+                        .queryParam("uuid", UUID.randomUUID().toString())
+                        .build())
+                .retrieve()
+                .bodyToMono(String.class);
+    }
+
+    @GetMapping("/webclient/ok")
+    public Mono<String> ok() {
+        return webClientBuilder
+                .baseUrl("http://localhost:8080")
+                .build()
+                .get()
+                .uri("/api/sample?uuid={uuid}", UUID.randomUUID().toString())
+                .retrieve()
+                .bodyToMono(String.class);
+    }
+
+    @GetMapping("/webclient/ok2")
+    public Mono<String> ok2() {
+        var uri = UriComponentsBuilder.fromPath("/api/sample")
+                .queryParam("uuid", "{uuid}")
+                .build().toUriString();
+
+        return webClientBuilder
+                .baseUrl("http://localhost:8080")
+                .build()
+                .get()
+                .uri(uri, UUID.randomUUID().toString())
+                .retrieve()
+                .bodyToMono(String.class);
+    }
+}
