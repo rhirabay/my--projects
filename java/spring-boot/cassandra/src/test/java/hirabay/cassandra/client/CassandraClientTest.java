86a3d13d5337c21f9b44fef736ba1fba1ddb10e6
rhirabay <rhirabay@yahoo-corp.jp>
2023-03-05 22:05:25 +0900

    add cassandra unit

--- java/spring-boot/cassandra/src/test/java/hirabay/cassandra/client/CassandraClientTest.java
+++ java/spring-boot/cassandra/src/test/java/hirabay/cassandra/client/CassandraClientTest.java
@@ -1,51 +1,29 @@
 package hirabay.cassandra.client;
 
-//import org.testcontainers.containers.CassandraContainer;
-import com.datastax.driver.core.Cluster;
-import com.datastax.driver.core.Session;
 import com.datastax.oss.driver.api.core.CqlSession;
 import hirabay.cassandra.domain.Sample;
+import org.cassandraunit.CQLDataLoader;
+import org.cassandraunit.dataset.cql.ClassPathCQLDataSet;
+import org.cassandraunit.utils.EmbeddedCassandraServerHelper;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.data.cassandra.core.CassandraTemplate;
-import org.testcontainers.containers.CassandraContainer;
-import org.testcontainers.containers.GenericContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
-@Testcontainers
 class CassandraClientTest {
-    @Container
-    public static final CassandraContainer cassandra
-            = (CassandraContainer) new CassandraContainer("cassandra:3.11.2").withExposedPorts(9042);
-
-    private CassandraTemplate template;
-
-    @BeforeEach
-    void setup() {
-        Cluster cluster = Cluster.builder()
-                .addContactPoints(cassandra.getHost())
-//                .withoutJMXReporting()  // 付けないとエラー
-                .build();
-        try (Session session = cluster.connect()) {
-            session.execute("""
-                            CREATE KEYSPACE IF NOT EXISTS sample
-                              WITH replication = {'class':'SimpleStrategy','replication_factor':'1'};
-                    """);
-        }
-
-        CqlSession session = CqlSession
-                .builder()
-                .addContactPoint(cassandra.getContactPoint())
-                .withLocalDatacenter(cassandra.getLocalDatacenter())
-                .build();
-
-        this.template = new CassandraTemplate(session);
-    }
-
+//    private CassandraTemplate template;
+//    private static CqlSession session;
+//
     @Test
-    void test() {
+    void test() throws Exception {
+        System.out.println("start cassandra");
+        EmbeddedCassandraServerHelper.startEmbeddedCassandra();
+        System.out.println("get cassandra session");
+        var session = EmbeddedCassandraServerHelper.getSession();
+        System.out.println("load cql");
+        new CQLDataLoader(session).load(new ClassPathCQLDataSet("sample.cql", "sample"));
+
+        var template = new CassandraTemplate(session);
         var actual = template.select("SELECT * FROM t_sample", Sample.class);
 
         System.out.println(actual);
