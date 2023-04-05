31ce3bb141abcba742f54912e1c5a815c7850112
rhirabay <rhirabay@yahoo-corp.jp>
2023-04-05 13:23:59 +0900

    add project

---
+++ java/spring-boot/springboot2-springboot3/springboot2/src/test/java/hirabay/springboot2springboot3/Springboot2Springboot3ApplicationTests.java
@@ -0,0 +1,13 @@
+package hirabay.springboot2springboot3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot2Springboot3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
