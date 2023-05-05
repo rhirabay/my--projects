90d40246e268a542665bddbe3a6ac5c538bcad96
rhirabay <rhirabay@yahoo-corp.jp>
2023-05-05 23:35:42 +0900

    testcontainers

--- java/spring-boot/testcontainers/cassandra/src/test/java/hirabay/testcontainers/ApplicationTest.java
+++ java/spring-boot/testcontainers/cassandra/src/test/java/hirabay/testcontainers/ApplicationTest.java
@@ -6,6 +6,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.context.ImportTestcontainers;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.CassandraContainer;
@@ -18,24 +20,26 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @Testcontainers
 @SpringBootTest
+@ImportTestcontainers(CassandraTestContainers.class)
 class ApplicationTest {
     @Autowired
     private CassandraClient cassandraClient;
 
 
     // テストコンテナを生成（裏でDockerコンテナが起動する）
-    @Container
-    public static CassandraContainer<?> cassandra = new CassandraContainer<>("cassandra:3.11.2")
-            .withInitScript("initial.cql"); // 初期クエリ
+//    @Container
+//    @ServiceConnection
+//    public static CassandraContainer<?> cassandra = new CassandraContainer<>("cassandra:3.11.2")
+//            .withInitScript("initial.cql"); // 初期クエリ
 
     // propertiesを更新
-    @DynamicPropertySource
-    static void properties(DynamicPropertyRegistry registry) {
-        registry.add("spring.cassandra.keyspace-name", () -> "sample");
-        var contactPoint = "%s:%d".formatted(cassandra.getContactPoint().getHostName(), cassandra.getContactPoint().getPort());
-        registry.add("spring.cassandra.contact-points", () -> contactPoint);
-        registry.add("spring.cassandra.local-datacenter", () -> cassandra.getLocalDatacenter());
-    }
+//    @DynamicPropertySource
+//    static void properties(DynamicPropertyRegistry registry) {
+//        registry.add("spring.cassandra.keyspace-name", () -> "sample");
+//        var contactPoint = "%s:%d".formatted(cassandra.getContactPoint().getHostName(), cassandra.getContactPoint().getPort());
+//        registry.add("spring.cassandra.contact-points", () -> contactPoint);
+//        registry.add("spring.cassandra.local-datacenter", () -> cassandra.getLocalDatacenter());
+//    }
 
     @Test
     void test() {
