687ec96cfa611f7e109a1fc659517926a5ee644e
rhirabay <rhirabay@yahoo-corp.jp>
2023-07-11 09:33:24 +0900

    add rabbitmq

--- java/spring-boot/webflux/sample/src/main/java/hirabay/sample/WebClientAutoConfiguration.java
+++ java/spring-boot/webflux/sample/src/main/java/hirabay/sample/WebClientAutoConfiguration.java
@@ -1,8 +1,10 @@
 package hirabay.sample;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class WebClientAutoConfiguration {
-
+    @Bean
+    public WebClient webClient
 }
