34ba441715227266ecdd0877e42984ecc6713591
rhirabay <rhirabay@yahoo-corp.jp>
2023-08-08 09:13:56 +0900

    update

--- java/spring-boot/webflux/sample/src/main/java/hirabay/sample/WebClientAutoConfiguration.java
+++ java/spring-boot/webflux/sample/src/main/java/hirabay/sample/WebClientAutoConfiguration.java
@@ -2,9 +2,12 @@ package hirabay.sample;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class WebClientAutoConfiguration {
-    @Bean
-    public WebClient webClient
+//    @Bean
+    public WebClient webClient(WebClient.Builder builder) {
+        return null;
+    }
 }
