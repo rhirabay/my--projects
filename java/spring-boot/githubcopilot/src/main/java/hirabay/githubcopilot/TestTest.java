6f904a84eb9ac1383b15bc26ab15f72ac03ebf00
rhirabay <ryo662233@gmail.com>
2023-08-30 21:26:56 +0900

    update

---
+++ java/spring-boot/githubcopilot/src/main/java/hirabay/githubcopilot/TestTest.java
@@ -0,0 +1,11 @@
+package hirabay.githubcopilot;
+
+public class TestTest {
+    // alert("こんにちは");
+    // これをJavaに変換すると
+    // alert("こんにちは");
+    // となる
+    public static void main(String[] args) {
+        System.out.println("こんにちは");
+    }
+}
---
+++ java/spring-boot/githubcopilot/src/test/java/hirabay/githubcopilot/GithubcopilotApplicationTests.java
@@ -0,0 +1,13 @@
+package hirabay.githubcopilot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubcopilotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
