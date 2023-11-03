b5d74b7da73ec240559a207881a10fe54a905952
rhirabay <rhirabay@yahoo-corp.jp>
2023-11-03 20:55:21 +0900

    update

---
+++ java/spring-boot/thymeleaf/app/src/main/java/hirabay/thymeleaf/controller/FormSampleController.java
@@ -0,0 +1,44 @@
+package hirabay.thymeleaf.controller;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.Value;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+@Slf4j
+@Controller
+public class FormSampleController {
+    @GetMapping("/form")
+    public String get(Model model) {
+        var form = new SampleForm("hirabay", List.of("programming", "reading"));
+
+        model.addAttribute("form", form);
+
+        return "form";
+    }
+
+    @PostMapping("/form")
+    public String post(@ModelAttribute SampleForm form, Model model) {
+        log.info("form: {}", form);
+        model.addAttribute("form", form);
+
+        return "form";
+    }
+
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class SampleForm {
+        private String name;
+        private List<String> hobbies;
+    }
+}
