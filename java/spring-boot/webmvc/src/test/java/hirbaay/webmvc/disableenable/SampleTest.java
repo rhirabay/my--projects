d7a984aa2900635a3e9f4385bcd04bcd390e4c8d
rhirabay <rhirabay@yahoo-corp.jp>
2023-11-25 20:59:29 +0900

    update

---
+++ java/spring-boot/webmvc/src/test/java/hirbaay/webmvc/disableenable/SampleTest.java
@@ -0,0 +1,21 @@
+package hirbaay.webmvc.disableenable;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.DisabledIfEnvironmentVariable;
+import org.junit.jupiter.api.condition.EnabledIf;
+import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable;
+import org.junit.jupiter.api.condition.EnabledIfSystemProperty;
+
+@DisabledIfEnvironmentVariable(named = "PR", matches = "true")
+public class SampleTest {
+    @Test
+    @EnabledIfSystemProperty(named = "custom-props", matches = "test")
+    void test1() {
+        System.out.println("test1");
+    }
+
+    @Test
+    void test2() {
+        System.out.println("test2");
+    }
+}
