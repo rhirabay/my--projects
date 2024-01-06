b5d59f18f202b06a157a5e26a5fd290dc48683e1
rhirabay <rhirabay@yahoo-corp.jp>
2024-01-06 23:28:20 +0900

    update

--- java/spring-boot/3.2/locust/locustfile.py
+++ java/spring-boot/3.2/locust/locustfile.py
@@ -3,8 +3,8 @@ from locust import task, constant_throughput, HttpUser
 class SampleUser(HttpUser):
     @task
     def get_request(self):
-        self.client.get('/virtual-thread/sleep')
+        self.client.get('/server?sleep=1')
 
     wait_time = constant_throughput(1)
     # （任意）UI上のデフォルトのホストを指定する
-    host = 'http://localhost:8081'
+    host = 'http://localhost:8080'
