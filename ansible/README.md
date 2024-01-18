ee17b46d9958a7ae0493a9ca3e7c111506f0cc14
rhirabay <rhirabay@yahoo-corp.jp>
2024-01-18 23:42:13 +0900

    update

--- ansible/README.md
+++ ansible/README.md
@@ -3,3 +3,7 @@
 ```shell
 ansible-playbook playbook.yml
 ```
+
+```shell
+ansible-playbook -i inventories.yml playbook.yml
+```
--- java/spring-boot/3.2/virtual-thread/src/main/java/hirabay/vt/VirtualThreadApplication.java
+++ java/spring-boot/3.2/virtual-thread/src/main/java/hirabay/vt/VirtualThreadApplication.java
@@ -2,7 +2,9 @@ package hirabay.vt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
+@EnableAsync
 @SpringBootApplication
 public class VirtualThreadApplication {
 
