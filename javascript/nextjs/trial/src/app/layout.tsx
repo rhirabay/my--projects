311aec80f1fc7bb6b5f47467b31ba04633ba7717
rhirabay <rhirabay@yahoo-corp.jp>
2024-02-08 10:49:50 +0900

    update

--- javascript/nextjs/trial/src/app/layout.tsx
+++ javascript/nextjs/trial/src/app/layout.tsx
@@ -3,7 +3,7 @@ import './globals.css'
 export default function RootLayout({children}: { children: React.ReactNode }) {
     return (
         <html lang="en">
-            <body>{children}</body>
+            <body className="min-h-screen flex flex-col items-center justify-center bg-gray-100">{children}</body>
         </html>
     )
 }
