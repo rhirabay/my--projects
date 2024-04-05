4945a9b06489f54f384d163978c83d12aa9bc410
rhirabay <rhirabay@yahoo-corp.jp>
2024-04-05 11:06:43 +0900

    update

---
+++ java/spring-boot/webmvc/src/main/java/hirabay/webmvc/controller/JsonAndXmlSupportedRestController.java
@@ -0,0 +1,24 @@
+package hirabay.webmvc.controller;
+
+import lombok.Data;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class JsonAndXmlSupportedRestController {
+    // curl localhost:8080/jsonXml -H "Accept: application/json"
+    // curl localhost:8080/jsonXml -H "Accept: application/xml"
+    @GetMapping(value = "/jsonXml", produces = {"application/xml", "application/json"})
+    public SampleModel jxonXml() {
+        var sampleModel = new SampleModel();
+        sampleModel.setId("1");
+        sampleModel.setName("sample");
+        return sampleModel;
+    }
+
+    @Data
+    public static class SampleModel {
+        private String id;
+        private String name;
+    }
+}
