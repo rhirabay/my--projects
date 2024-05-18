3d7ff127763c482f4e7550cc6266a8c8457f0020
rhirabay <rhirabay@yahoo-corp.jp>
2024-05-18 23:11:50 +0900

    update

--- python/sqlalchemy/src/postgres/repository/employee_repository.py
+++ python/sqlalchemy/src/postgres/repository/employee_repository.py
@@ -1,39 +1,11 @@
-import sqlalchemy as sa
-from sqlalchemy.orm import Session
-from sqlalchemy.ext.declarative import declarative_base
-
-from src.postgres.model.base import Base
 from src.postgres.model.employee import Employee
+from src.postgres.repository.datasource import create_session
 
 
 class EmployeeRepository:
     def __init__(self):
-        self.engine = sa.create_engine(
-            sa.engine.url.URL.create(
-                drivername="postgresql",
-                username="postgres",
-                password='password',
-                host='localhost',
-                port=5432,
-                database='sampledb',
-            )
-        )
-        self.session = self.create_session()
+        self.session = create_session()
 
-    def create_table(self):
-        Employee.metadata.create_all(bind=self.engine)
-
-    def create_session(self):
-        from sqlalchemy.orm import scoped_session, sessionmaker
-
-        # Sessionの作成
-        return scoped_session(
-            sessionmaker(
-                autocommit=False,
-                autoflush=False,
-                bind=self.engine
-            )
-        )
 
     def add(self, employee: Employee) -> Employee:
         self.session.add(employee)
