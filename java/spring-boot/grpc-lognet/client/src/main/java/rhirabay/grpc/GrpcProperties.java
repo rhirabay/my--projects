9ba007822a2d0700e92d7e3a920e36c42843fea0
rhirabay <rhirabay@yahoo-corp.jp>
2024-12-08 15:40:42 +0900

    3.3 to 3.4

--- java/spring-boot/grpc-lognet/client/src/main/java/rhirabay/grpc/GrpcProperties.java
+++ java/spring-boot/grpc-lognet/client/src/main/java/rhirabay/grpc/GrpcProperties.java
@@ -1,17 +1,23 @@
 package rhirabay.grpc;
 
+import jakarta.annotation.Nonnull;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
+import jakarta.validation.Valid;
 
 import java.util.Map;
 
 @Data
+@Validated
 @ConfigurationProperties("rhirabay.grpc")
 public class GrpcProperties {
+    @Valid
     private Map<String, GrpcClientProperties> client;
 
     @Data
     public static class GrpcClientProperties {
+        @Nonnull
         private String host;
         private int port;
     }
