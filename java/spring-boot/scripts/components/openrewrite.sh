7f776ba276115dafa5541b02a6212767e8d12a49
rhirabay <rhirabay@yahoo-corp.jp>
2024-12-08 16:11:05 +0900

    rewrite準備

---
+++ java/spring-boot/scripts/components/openrewrite.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+
+###########################################
+###   OpenRewrite
+###########################################
+function checkEnvironment() {
+    if [ -z "${REWRITE_YML_FILE}" ]; then
+      echo "REWRITE_YML_FILE is not set."
+      exit 1
+    fi
+    if [ -z "${OPENREWRITE_PLUGIN_VERSION}" ]; then
+      echo "OPENREWRITE_PLUGIN_VERSION is not set."
+      exit 1
+    fi
+    if [ -z "${OPENREWRITE_REWRITE_SPRING_LIB_VERSION}" ]; then
+      echo "OPENREWRITE_REWRITE_SPRING_LIB_VERSION is not set."
+      exit 1
+    fi
+}
+
+function cleanOpenRewrite() {
+  for build_gradle_file in ${build_gradle_files[@]}; do
+    # rewrite.yml削除
+    rm $(dirname ${build_gradle_file})/rewrite.yml
+    # 依存ライブラリ削除
+    dependencies_block_start=$(${sed_cmd} -n '/^dependencies {/=' ${build_gradle_file})
+    dependencies_block_end=$(${sed_cmd} -n '/^}/=' ${build_gradle_file} | awk '{ if ($0 >= '${dependencies_block_start}') print $0 }' | head -1)
+    ${sed_cmd} -i "${dependencies_block_start},${dependencies_block_end}{/[ ]*rewrite/d;}" ${build_gradle_file}
+    # プラグイン削除
+    plugin_block_start=$(${sed_cmd} -n '/^plugins {/=' ${build_gradle_file})
+    plugin_block_end=$(${sed_cmd} -n '/^}/=' ${build_gradle_file} | awk '{ if ($0 >= '$plugin_block_start') print $0 }' | head -1)
+    ${sed_cmd} -i "${plugin_block_start},${plugin_block_end}{/.*rewrite/d;}" ${build_gradle_file}
+    # rewriteブロック削除
+    grep "^rewrite" ${build_gradle_file}
+    if [ $? -eq 0 ]; then
+      rewrite_block_start=$(${sed_cmd} -n '/^rewrite {/=' ${build_gradle_file})
+      rewrite_block_end=$(${sed_cmd} -n '/^}/=' ${build_gradle_file} | awk '{ if ($0 >= '$rewrite_block_start') print $0 }' | head -1)
+      ${sed_cmd} -i "${rewrite_block_start},${rewrite_block_end}d" ${build_gradle_file}
+    fi
+  done
+}
+
+function setupOpenRewrite() {
+    for build_gradle_file in ${build_gradle_files[@]}; do
+      # pluginブロックがない場合はスキップ
+      grep "^plugins" ${build_gradle_file}
+      if [ $? -ne 0 ]; then
+        continue
+      fi
+      # dependenciesブロックがない場合はスキップ
+      grep "^dependencies" ${build_gradle_file}
+      if [ $? -ne 0 ]; then
+        continue
+      fi
+
+      # ファイル追加
+      cp ${REWRITE_YML_FILE} $(dirname ${build_gradle_file})/rewrite.yml
+      # プラグイン追加
+      grep 'org.openrewrite.rewrite' ${build_gradle_file}
+      if [ $? -ne 0 ]; then
+        plugin_block_start=$(${sed_cmd} -n '/^plugins {/=' ${build_gradle_file})
+        plugin_block_end=$(${sed_cmd} -n '/}/=' ${build_gradle_file} | awk '{ if ($0 >= '$plugin_block_start') print $0 }' | head -1)
+        ${sed_cmd} -i -e "${plugin_block_end}i \ \ \ \ id('org.openrewrite.rewrite') version('${OPENREWRITE_PLUGIN_VERSION}')" ${build_gradle_file}
+      fi
+      # 依存を追加
+      dependencies_block_start=$(${sed_cmd} -n '/^dependencies {/=' ${build_gradle_file})
+      dependencies_block_end=$(${sed_cmd} -n '/^}/=' ${build_gradle_file} | awk '{ if ($0 >= '${dependencies_block_start}') print $0 }' | head -1)
+      grep 'rewrite-spring' ${build_gradle_file}
+      if [ $? -ne 0 ]; then
+        ${sed_cmd} -i -e "${dependencies_block_end}i \ \ \ \ rewrite('org.openrewrite.recipe:rewrite-spring:${OPENREWRITE_REWRITE_SPRING_LIB_VERSION}')" ${build_gradle_file}
+        dependencies_block_end=$((${dependencies_block_end} + 1))
+      fi
+
+      # rewriteブロックを追加
+      echo '' >> ${build_gradle_file}
+      echo 'rewrite {' >> ${build_gradle_file}
+      echo "    activeRecipe('springboot.migration')" >> ${build_gradle_file}
+      echo '}' >> ${build_gradle_file}
+    done
+}
+
+function executeOpenRewrite() {
+    cleanOpenRewrite
+    setupOpenRewrite
+    ./gradlew -q rewriteRun
+    if [ $? -ne 0 ]; then
+      echo "error on rewriteRun"
+      exit 1
+    fi
+    cleanOpenRewrite
+}
