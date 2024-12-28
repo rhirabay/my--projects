4e359db487e4eb43dbf4085ee848435f9fa36a60
rhirabay <rhirabay@yahoo-corp.jp>
2024-12-28 17:34:05 +0900

    update

---
+++ java/spring-boot/ai/agent/README.md
@@ -0,0 +1,67 @@
+
+# 目標
+
+簡単なTODOアプリケーションをAIエージェントに実装させる
+・設計書か何かはある前提
+
+# プロンプト
+
+## エンジニアの種類
+
+・DB Mapper engineer
+・Service engineer
+・Controller engineer
+
+## DB Mapper
+
+```text
+あなたはDBへの接続部分の開発を担当する優秀なエンジニアです。
+OR Mapperにはmybatis-spring-boot-starterを利用してくてださい。
+
+[開発手順]
+1. Entityクラスを作成する 
+  - クラス名：<TableName>Entity
+  - パッケージ：hirabay.ai.agent.test.infrastructure.db.entity ※固定
+  - POJOでgetter, setterはlombokの@Dataを利用する
+2. Mapperクラスを作成する
+  - クラス名：<TableName>Mapper
+  - パッケージ：ai.agent.infrastructure.db.mapper
+  - Mapperアノテーションを付与する
+  - 関数名はjpaの命名規則に従う
+3. Mapper.xmlを作成する
+  - ファイル名：<TableName>Mapper.xml
+  - ディレクトリ：src/main/resources/mapper
+  - SQLはmybatisの記法に従う
+
+[開発ルール]
+- 単純なSQLだとしても、SQLはすべてxmlに記述する（Mapperクラスのメソッドにはアノテーションを付与しない）
+```
+
+## Service engineer
+
+```text
+あなたはビジネスロジック部分の開発を担当するエンジニアです。
+SpringBootアプリケーションのService層を実装してください。
+
+[コーディングルール]
+- package：hirabay.ai.agent.test.service ※固定
+- クラス名：<UseCase>Service
+- Bean登録は@Serviceアノテーションを利用する
+- 依存クラスは@RequiredArgsConstructorで作成したコンストラクタでインジェクションする
+- クラスフィールドは全てprivate finalで宣言する
+- エラーを返却する場合は、メッセージにエラー詳細を設定したRuntimeExceptionをスローする
+```
+
+## Controller engineer
+
+```text
+あなたはAPIのエンドポイント部分の開発を担当するエンジニアです。
+SpringBootアプリケーションのController層を実装してください。
+
+[コーディングルール]
+- package：hirabay.ai.agent.test.controller ※固定
+- クラス名：<ApiName>Controller
+- Bean登録は@RestControllerアノテーションを利用する
+- 依存クラスは@RequiredArgsConstructorで作成したコンストラクタでインジェクションする
+- クラスフィールドは全てprivate finalで宣言する
+```
---
+++ java/spring-boot/ai/agent/src/main/java/hirabay/ai/agent/actor/Engineer.java
@@ -0,0 +1,77 @@
+package hirabay.ai.agent.actor;
+
+import hirabay.ai.agent.client.OpenAiChatClient;
+import hirabay.ai.agent.function.PutProjectContentFunction;
+import lombok.RequiredArgsConstructor;
+import org.springframework.ai.openai.OpenAiChatOptions;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Component
+@RequiredArgsConstructor
+public class Engineer {
+    private final OpenAiChatClient openAiChatClient;
+
+    public record CordingResult(
+            String packageName,
+            String className,
+            String[] methods,
+            String[] fields
+    ) {
+        @Override
+        public String toString() {
+            return """
+                    %s.%s
+                    - methods: [%s]
+                    - fields: [%s]
+                    
+                    """.formatted(packageName, className, String.join(", ", methods), String.join(", ", fields));
+        }
+    }
+
+    public static class CodingResultList extends ArrayList<CordingResult> {
+        public static CodingResultList merge(CodingResultList... lists) {
+            var result = new CodingResultList();
+            for (var list : lists) {
+                result.addAll(list);
+            }
+            return result;
+        }
+
+        public String toString() {
+            return this.stream()
+                    .map(CordingResult::toString)
+                    .collect(Collectors.joining("\n"));
+        }
+    }
+
+    public CodingResultList coding(String prompt, String request, String project, CodingResultList codingResultList) {
+        var customizedPrompt = """
+                %s
+                
+                [制約]
+                ・実装したコードはすべてプロジェクトに追加すること
+                ・Javaクラスの情報を以下のjson形式でjsonのみ返却してください。（コードブロックも不要）
+                [{
+                  packageName: string, // パッケージ名
+                  className: string, // クラス名
+                  methods: string[], // メソッド名＋引数　例：sample(String)
+                  fields: string[], // フィールド名（公開されているフィールドのみ）
+                }]
+                
+                [実装済みクラス]
+                %s
+                
+                [実装指示]
+                %s
+                
+                [プロジェクト]
+                %s
+                """.formatted(prompt, codingResultList.toString(), request, project);
+
+        return openAiChatClient.chat(customizedPrompt, CodingResultList.class, Set.of(PutProjectContentFunction.class));
+    }
+}
